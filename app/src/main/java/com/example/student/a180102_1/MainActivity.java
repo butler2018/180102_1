31b65252fe8bbfdcda40ee9e0bf8076fb340825d
calvin <butler2018@hotmail.com>
2018-01-02 10:28:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/a180102_1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.student.a180102_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click1(View v)
+    {
+        Toast.makeText(MainActivity.this,"TEST TEST TEST",Toast.LENGTH_LONG).show();
+    }
+
+
+}
